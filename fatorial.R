a655e3ecd833efb86b8540d2d720d059fb84c1a6
Gabriela Paschoal <45124512+gabrielabjn@users.noreply.github.com>
2025-02-26 13:24:54 -0300

    Update fatorial.R

--- fatorial.R
+++ fatorial.R
@@ -6,36 +6,36 @@ data<-read_excel('Fatorial.xlsx')
 
 I <- 5 # numero de niveis fator 1 (aparelho)
 J <- 4 # numero de niveis fator 2 (operador)
-K<- 10 # numero de blocos (arvores)
+R <- 10 # numero de blocos (arvores)
 
 # GL ---------------
 GL_1<- I - 1
 GL_2<- J-1
 GL_int<- GL_1*GL_2
 GL_trat<-I*J-1
-GL_b<-K-1
-GL_tot<-I*J*K-1
+GL_b<-R-1
+GL_tot<-I*J*R-1
 GL_res<-GL_tot-GL_b-GL_trat
 
 # SQ ----------------
 
-C<-1/(I*J*K)*sum(data$obs)^2
+C<-1/(I*J*R)*sum(data$obs)^2
 
 # Aparelho (fator 1)
 aparelho <- aggregate(obs ~ aparelho, data = data, FUN = sum)
-SQ1<-(1/(J*K))* sum(aparelho$obs^2) - C
+SQ1<-(1/(J*R))* sum(aparelho$obs^2) - C
 
 # Operador (fator 2)
 op <- aggregate(obs ~ operador, data = data, FUN = sum)
-SQ2<-(1/(I*K))* sum(op$obs^2) - C
+SQ2<-(1/(I*R))* sum(op$obs^2) - C
 
 # (Tratamento)
 ap_op<-aggregate(obs~aparelho+operador,data=data,FUN=sum)
-SQ4<-1/K*sum(ap_op$obs^2)-C
+SQ4<-1/R*sum(ap_op$obs^2)-C
 
 # Interacao
 SQ3<-SQ4-SQ1-SQ2
-  
+
 # Blocos
 blocos<-aggregate(obs~bloco,data=data,FUN=sum)
 SQ5<-1/(I*J)*sum(blocos$obs^2)-C
@@ -45,7 +45,7 @@ SQ7<-sum(data$obs^2)-C
 
 # Residuos
 SQ6<-SQ7-SQ4-SQ5
-  
+
 # QM ----------------
 
 QM1<-SQ1/GL_1 
@@ -90,7 +90,7 @@ tot_ap <-aggregate(obs ~ aparelho,data=ap_op,FUN=sum)$obs
 # Op. d. Ap ---------------------------------
 k<-5
 x<-ap_op[ap_op$aparelho==k,]
-SQ<-(1/K)*sum(x$obs^2)-(sum(x$obs)^2)*1/(J*K)
+SQ<-(1/R)*sum(x$obs^2)-(sum(x$obs)^2)*1/(J*R)
 SQ
 QM<-SQ/GL_2
 QM
@@ -101,7 +101,7 @@ qf(0.95,GL_2,GL_res)
 # Ap. d. Op ----------------------------------
 k<-4
 x<-ap_op[ap_op$operador==k,]
-SQ<-(1/K)*sum(x$obs^2)-(sum(x$obs)^2)*1/(I*K)
+SQ<-(1/R)*sum(x$obs^2)-(sum(x$obs)^2)*1/(I*R)
 SQ
 QM<-SQ/GL_1
 QM
